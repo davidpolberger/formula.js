204d2289f3fab3b7291f5b9605ffc4553eb07705
joaojeronimo <jj@crowdprocess.com>
2014-10-13 15:39:49 +0100

    Add TRANSPOSE function

---
+++ test/lookup-reference.js
@@ -0,0 +1,7 @@
+var lookupAndReference = require('../lib/lookup-reference');
+
+suite('Lookup & Reference', function() {
+  test('TRANSPOSE', function() {
+    lookupAndReference.TRANSPOSE([[1,2],[3,4]]).should.eql([[1,3],[2,4]]);
+  });
+});
