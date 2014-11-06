520270855252d57a58adedc4e2116b69f880eef8
Miguel Serrano <miguelvps@gmail.com>
2014-11-06 15:50:47 +0000

    Allow invalid values in SUMIF

--- utils/index.js
+++ utils/index.js
@@ -53,7 +53,7 @@ exports.parseBool = function(bool) {
 };
 
 exports.parseNumber = function(string) {
-  if (string === undefined || string === '') {
+  if (string === undefined || string === null || string === '') {
     return error.value;
   }
   if (!isNaN(string)) {
